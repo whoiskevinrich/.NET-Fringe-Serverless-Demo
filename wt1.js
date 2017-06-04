aaedb47d6e5f7dbae527db1fd8c1e27b7bf9000e
Kevin Rich <whoiskevinrich@gmail.com>
2017-06-04 11:05:26 -0700

    storage section

--- wt1.js
+++ wt1.js
@@ -1,7 +1,13 @@
 module.exports = function(ctx, cb) {
   var slack = require("slack-notify")(ctx.secrets.SLACK_URL);
   var body = ctx.body;
-  if (body.issue && body.action === "opened") {
+  var attempts;
+  
+  if (ctx.data.showstats === "true") {
+    return getStats();
+  }
+  else if (body.issue && body.action === "opened") {
+    console.log("issue created");
     var issue = body.issue;
 
     var text='*New Issue*\n\n' + 
@@ -10,8 +16,41 @@ module.exports = function(ctx, cb) {
              `Url: ${issue.url}\n` +
              `Title: ${issue.title}\n\n` +
              `${issue.body}`;
-
-    slack.send({text:text, username: "webtask-bot", icon_emoji: ":robot_face:"});   
+             
+    slack.send({text:text, username: "webtask-bot", icon_emoji: ":robot_face:"}); 
+    incrementCounter();
   }
-  cb();
-};
+
+  function incrementCounter() {
+    ctx.storage.get(function(error, data){
+      if (data === undefined) {
+        data={};
+      }
+      var repoName = body.repository.full_name
+      data[repoName] === undefined ? data[repoName] = 1 : data[repoName]++;
+      attempts = 3 ;
+      ctx.storage.set(data, function(error) {
+        setStorage(error, data);
+      });
+    });
+  }
+  
+  function setStorage(error,data) {
+    if(error) {
+      if (error.code == 409 && attempts--) {
+        data.counter = Math.max(data.counter, error.conflict.counter) + 1;
+        return ctx.storage.set(data, setStorage);
+      }
+      else {
+        return cb(error);
+      }
+    }
+    cb();
+  }
+  
+  function getStats() {
+    ctx.storage.get(function(error,data){
+      cb(null, data); 
+    });
+  }
+}; 
