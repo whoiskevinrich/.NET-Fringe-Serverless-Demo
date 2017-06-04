2761d1abeff731db5173f72f465a35aaafc9eb45
Kevin Rich <whoiskevinrich@gmail.com>
2017-06-04 10:42:27 -0700

    Update wt1.js

--- wt1.js
+++ wt1.js
@@ -1,4 +1,7 @@
-var util = require('util');
+//var util = require('util');
+
+import util from "util";
+
 module.exports = function(ctx, cb) {
   //console.log(util.inspect(ctx.body, {depth:null}));
   console.log(util.inspect(ctx.body.issue.url));
