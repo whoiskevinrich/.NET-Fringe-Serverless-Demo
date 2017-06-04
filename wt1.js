cbd99cb993488e55023476ab4e114fb1470effae
Kevin Rich <whoiskevinrich@gmail.com>
2017-06-04 10:41:54 -0700

    Update wt1.js

--- wt1.js
+++ wt1.js
@@ -1,6 +1,6 @@
 var util = require('util');
 module.exports = function(ctx, cb) {
-  console.log(util.inspect(ctx.body, {depth:null}));
+  //console.log(util.inspect(ctx.body, {depth:null}));
   console.log(util.inspect(ctx.body.issue.url));
   cb();
 };
